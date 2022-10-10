240c58c12db3f16f7815548e9eb1daaee6988179
Ankur Jhavery <ajhavery@gmail.com>
2022-10-10 21:08:54 +0530

    Initial Commit

--- app/src/main/java/com/example/apptutorial/MainActivity.kt
+++ app/src/main/java/com/example/apptutorial/MainActivity.kt
@@ -3,16 +3,46 @@ package com.example.apptutorial
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import com.example.apptutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var textAnimText: TextView
+    private lateinit var translate: Animation
+    private lateinit var rotate: Animation
+    private lateinit var scale: Animation
+    private lateinit var alpha: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
+
+        textAnimText = activityMainBinding.textAnimText
+
+        activityMainBinding.btnTranslate.setOnClickListener {
+            translate = AnimationUtils.loadAnimation(this, R.anim.translate)
+            textAnimText.startAnimation(translate)
+        }
+
+        activityMainBinding.btnRotate.setOnClickListener{
+            rotate = AnimationUtils.loadAnimation(this, R.anim.rotate)
+            textAnimText.startAnimation(rotate)
+        }
+
+        activityMainBinding.btnScale.setOnClickListener{
+            scale = AnimationUtils.loadAnimation(this, R.anim.scale)
+            textAnimText.startAnimation(scale)
+        }
+
+        activityMainBinding.btnAlpha.setOnClickListener{
+            alpha = AnimationUtils.loadAnimation(this, R.anim.alpha)
+            textAnimText.startAnimation(alpha)
+        }
 
     }
 }
